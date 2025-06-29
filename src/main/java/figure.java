f4191b8f16141e1bdd196dbf37d36ed891dc101d
Marian Milan <marian.milan6@gmail.com>
2025-06-29 13:44:56 +0200

    Abstract class for a chess figure

--- src/main/java/figure.java
+++ src/main/java/figure.java
@@ -1,4 +1,19 @@
 package src.main.java;
 
-public class figure {
+public abstract class figure {
+
+    private boolean white = false;
+    private boolean dead = false;
+
+    public figure(boolean white){
+        this.white = white;
+    }
+
+    public void killed(){
+        this.dead = true;
+    }
+
+    public boolean isWhite(){
+        return this.white;
+    }
 }
