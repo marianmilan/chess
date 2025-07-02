654f842067c68d32dd78e233eafb3cfced7f0763
Marian Milan <marian.milan6@gmail.com>
2025-07-02 11:59:25 +0200

    .

--- src/main/java/model/figures/King.java
+++ src/main/java/model/figures/King.java
@@ -1,10 +1,17 @@
 package model.figures;
 
+import model.Board;
 import model.Position;
 
 public class King extends Figure {
 
     public King(boolean white, Position position){
         super(white, position);
+    }
+
+
+    @Override
+    public boolean isValidMove(Board board, Position whereTo) {
+
     }
 }
--- src/main/java/model/figures/Rook.java
+++ src/main/java/model/figures/Rook.java
@@ -1,10 +1,30 @@
 package model.figures;
 
+import model.Board;
 import model.Position;
 
 public class Rook extends Figure {
 
     public Rook(boolean white, Position position){
         super(white, position);
+    }
+
+    @Override
+    public boolean isValidMove(Board board, Position whereTo){
+        Position start = this.getPosition();
+
+        int rowDiff = start.yAbsPosDifference(whereTo);
+        int colDiff = start.xAbsPosDifference(whereTo);
+
+        int xDirection = start.getPosX() < whereTo.getPosX() ? 1 : -1;
+        int yDirection = start.getPosY() < whereTo.getPosY() ? 1 : -1;
+
+        // Check if the line is not straight
+        if(rowDiff != 0 && colDiff != 0){
+            return false;
+        }
+
+
+
     }
 }
