7fa6a8faf27723bb051dad1fbb309a7dbd9f0cfd
Marian Milan <marian.milan6@gmail.com>
2025-07-07 15:41:41 +0200

    add movement highlighting

--- src/main/java/GUI/GameWindow.java
+++ src/main/java/GUI/GameWindow.java
@@ -9,9 +9,20 @@ public class GameWindow extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Chess");
         setLayout(new BorderLayout());
+        add(addGameMenu(), BorderLayout.NORTH);
         add(windowPanel, BorderLayout.CENTER);
         setSize(1024, 820);
         setLocationRelativeTo(null);
         setVisible(true);
+    }
+
+    public JMenuBar addGameMenu(){
+        JMenuBar menuBar = new JMenuBar();
+        JMenu gameMenu = new JMenu("Game");
+        JMenuItem menuItem = new JMenuItem("Reset");
+        gameMenu.add(menuItem);
+        menuBar.add(gameMenu);
+        setJMenuBar(menuBar);
+        return menuBar;
     }
 }
--- src/main/java/GUI/WindowPanel.java
+++ src/main/java/GUI/WindowPanel.java
@@ -1,92 +1,92 @@
 package GUI;
 
+import Logic.GameManager;
+import model.Position;
+import model.figures.Piece;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
 public class WindowPanel extends JPanel {
     private final Square[][] squares = new Square[8][8];
+    private final PieceIconManager iconManager;
+    private final GameManager manager;
 
     public WindowPanel(){
         setLayout(new GridBagLayout());
+        this.manager = new GameManager();
+        this.iconManager = new PieceIconManager();
         setupChessBoard();
-        setupPieces();
+        refreshBoard();
     }
 
     private void setupChessBoard(){
-        int index = 0;
         GridBagConstraints grid = new GridBagConstraints();
         for(int i = 0; i < 8; i++){
             for(int j = 0; j < 8; j++){
-               Color color = index % 2 == 0 ? Color.WHITE : Color.DARK_GRAY;
+               Color color = (i + j) % 2 == 0 ? Color.WHITE : Color.DARK_GRAY;
                grid.gridx = i;
                grid.gridy = j;
-               squares[i][j] = new Square(color);
+               squares[i][j] = new Square(color, i, j);
+               squares[i][j].addActionListener(event -> {
+                   Square clicked = (Square) event.getSource();
+                   highlightSquares(clicked);
+               });
                add(squares[i][j], grid);
-               index++;
             }
-            index++;
         }
     }
 
-    public void setupPieces(){
-        ImageIcon whitePawn = new ImageIcon("src/main/resources/white_pawn.png");
-        ImageIcon blackPawn = new ImageIcon("src/main/resources/black_pawn.png");
-
-        ImageIcon whiteKing = new ImageIcon("src/main/resources/white_king.png");
-        ImageIcon blackKing = new ImageIcon("src/main/resources/black_king.png");
-
-        ImageIcon whiteQueen = new ImageIcon("src/main/resources/white_queen.png");
-        ImageIcon blackQueen = new ImageIcon("src/main/resources/black_queen.png");
-
-        ImageIcon whiteBishop = new ImageIcon("src/main/resources/white_bishop.png");
-        ImageIcon blackBishop = new ImageIcon("src/main/resources/black_bishop.png");
-
-        ImageIcon whiteKnight = new ImageIcon("src/main/resources/white_knight.png");
-        ImageIcon blackKnight = new ImageIcon("src/main/resources/black_knight.png");
-
-        ImageIcon whiteRook = new ImageIcon("src/main/resources/white_rook.png");
-        ImageIcon blackRook = new ImageIcon("src/main/resources/black_rook.png");
-
-        for(int i = 0; i < 8; i++){
-            squares[i][6].setIcon(whitePawn);
-            squares[i][1].setIcon(blackPawn);
+    public void refreshBoard() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                Color color = (i + j) % 2 == 0 ? Color.WHITE : Color.DARK_GRAY;
+                squares[i][j].setBackground(color);
+                Piece piece = manager.getBoard().getFigureOnSquare(new Position(i, j));
+                if (piece != null) {
+                    iconManager.displayIcon(squares[i][j], piece);
+                }
+            }
         }
+    }
 
-        squares[0][7].setIcon(whiteRook);
-        squares[7][7].setIcon(whiteRook);
-        squares[1][7].setIcon(whiteKnight);
-        squares[6][7].setIcon(whiteKnight);
-        squares[2][7].setIcon(whiteBishop);
-        squares[5][7].setIcon(whiteBishop);
-        squares[3][7].setIcon(whiteQueen);
-        squares[4][7].setIcon(whiteKing);
+    public void highlightSquares(Square square){
+        refreshBoard();
+        Piece currentPiece = manager.getBoard().getFigureOnSquare(new Position(square.posX, square.posY));
 
-        squares[0][0].setIcon(blackRook);
-        squares[7][0].setIcon(blackRook);
-        squares[1][0].setIcon(blackKnight);
-        squares[6][0].setIcon(blackKnight);
-        squares[2][0].setIcon(blackBishop);
-        squares[5][0].setIcon(blackBishop);
-        squares[3][0].setIcon(blackQueen);
-        squares[4][0].setIcon(blackKing);
+        if(currentPiece != null){
+            List<Position> moves = currentPiece.getPossibleMoves(manager.getBoard());
+            moves.stream()
+                    .forEach(position -> {
+                        squares[position.getPosX()][position.getPosY()].setBackground(new Color(255, 165, 0, 150));
+                    });
+        }
     }
 
     // Helper class for chess square
-    private class Square extends JButton{
+    public static class Square extends JButton {
+        private int posX;
+        private int posY;
 
-        public Square(Color color) {
+        public Square(Color color, int posX, int posY) {
+            this.posX = posX;
+            this.posY = posY;
+
             setContentAreaFilled(false);
             setBorderPainted(false);
-            // change this after
-            setFocusPainted(true);
+            setFocusPainted(false);
             setBackground(color);
             setOpaque(true);
+            setRolloverEnabled(false);
         }
-
 
         @Override
         public Dimension getPreferredSize(){
             return new Dimension(80,80);
         }
+
     }
 }
