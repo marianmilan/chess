17b52d9133d3be50be97463f574d7327e01b0382
Marian Milan <marian.milan6@gmail.com>
2025-07-07 15:08:14 +0200

    fixed bug with coordinates

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -20,55 +20,55 @@ public class Board {
 
     private void placePawns(){
         for(int i = 0; i < 8; i++){
-            board[6][i] = new Pawn(true, new Position(6, i));
-            board[1][i] = new Pawn(false, new Position(1, i));
+            board[i][6] = new Pawn(true, new Position(i, 6));
+            board[i][1] = new Pawn(false, new Position(i, 1));
         }
     }
 
     private void placeRooks(){
         // Place black rooks
         board[0][0] = new Rook(false, new Position(0,0));
-        board[0][7] = new Rook(false, new Position(0, 7));
+        board[7][0] = new Rook(false, new Position(7, 0));
 
         // Place white rooks
-        board[7][0] = new Rook(true, new Position(7, 0));
+        board[0][7] = new Rook(true, new Position(0, 7));
         board[7][7] = new Rook(true, new Position(7, 7));
     }
 
     private void placeKnights(){
         // Place black knights
-        board[0][1] = new Knight(false, new Position(0,1));
-        board[0][6] = new Knight(false, new Position(0, 6));
+        board[1][0] = new Knight(false, new Position(1,0));
+        board[6][0] = new Knight(false, new Position(6, 0));
 
         // Place white knights
-        board[7][1] = new Knight(true, new Position(7, 1));
-        board[7][6] = new Knight(true, new Position(7, 6));
+        board[1][7] = new Knight(true, new Position(1, 7));
+        board[6][7] = new Knight(true, new Position(6, 7));
     }
 
     private void placeBishops(){
         // Place black bishops
-        board[0][2] = new Bishop(false, new Position(0, 2));
-        board[0][5] = new Bishop(false, new Position(0, 5));
+        board[2][0] = new Bishop(false, new Position(2, 0));
+        board[5][0] = new Bishop(false, new Position(5, 0));
 
         // Place white bishops
-        board[7][2] = new Bishop(true, new Position(7, 2));
-        board[7][5] = new Bishop(true, new Position(7, 5));
+        board[2][7] = new Bishop(true, new Position(2, 7));
+        board[5][7] = new Bishop(true, new Position(5, 7));
     }
 
     private void placeQueens(){
         // Place black queen
-        board[0][3] = new Queen(false, new Position(0, 3));
+        board[3][0] = new Queen(false, new Position(3, 0));
 
         // Place white queen
-        board[7][3] = new Queen(true, new Position(7, 3));
+        board[3][7] = new Queen(true, new Position(3, 7));
     }
 
     private void placeKings(){
         // Place black king
-        board[0][4] = new King(false, new Position(0, 4));
+        board[4][0] = new King(false, new Position(4, 0));
 
         // Place white king
-        board[7][4] = new King(true, new Position(7, 4));
+        board[4][7] = new King(true, new Position(4, 7));
     }
 
     public Piece[][] getBoard(){
