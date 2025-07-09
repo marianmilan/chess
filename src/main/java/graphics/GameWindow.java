00eb3f35a1390e3ff914ee9017fa28437aebc787
Marian Milan <marian.milan6@gmail.com>
2025-07-09 08:35:57 +0200

    delete method

--- src/main/java/graphics/GameWindow.java
+++ src/main/java/graphics/GameWindow.java
@@ -4,25 +4,13 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameWindow extends JFrame {
-
-    public GameWindow(JPanel windowPanel){
+    public GameWindow(WindowPanel windowPanel){
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Chess");
         setLayout(new BorderLayout());
-        add(addGameMenu(), BorderLayout.NORTH);
         add(windowPanel, BorderLayout.CENTER);
         setSize(1024, 820);
         setLocationRelativeTo(null);
         setVisible(true);
-    }
-
-    public JMenuBar addGameMenu(){
-        JMenuBar menuBar = new JMenuBar();
-        JMenu gameMenu = new JMenu("Game");
-        JMenuItem menuItem = new JMenuItem("Reset");
-        gameMenu.add(menuItem);
-        menuBar.add(gameMenu);
-        setJMenuBar(menuBar);
-        return menuBar;
     }
 }
