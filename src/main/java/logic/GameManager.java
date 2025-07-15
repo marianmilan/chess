3cc3daeaaca9ccf96687cba2a88fb146dd036c8a
Marian Milan <marian.milan6@gmail.com>
2025-07-15 12:13:57 +0200

    add reset game

--- src/main/java/logic/GameManager.java
+++ src/main/java/logic/GameManager.java
@@ -3,7 +3,6 @@ package logic;
 import model.Board;
 import model.MoveResult;
 import model.Position;
-import model.figures.Piece;
 import model.figures.PieceType;
 
 public class GameManager {
@@ -20,17 +19,26 @@ public class GameManager {
         MoveResult result = board.movePiece(start, targetSquare, whiteTurn);
         if(result == MoveResult.VALID){
             whiteTurn = !whiteTurn;
+
+            if(board.staleMate(whiteTurn)){
+                return MoveResult.STALE_MATE;
+            }
+
             if(board.checkMate(whiteTurn)){
                 return MoveResult.CHECK_MATE;
             }
         }
-
         return result;
     }
 
     public void makePromotion(PieceType type, int posX, int posY){
         board.promotion(type, whiteTurn, posX, posY);
         whiteTurn = !whiteTurn;
+    }
+
+    public void resetGame(){
+        this.board = new Board();
+        this.whiteTurn = true;
     }
 
     public boolean isWhiteTurn(){
@@ -40,6 +48,4 @@ public class GameManager {
     public Board getBoard(){
         return this.board;
     }
-
-
 }
