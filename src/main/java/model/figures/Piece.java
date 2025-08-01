f7d2205e4f97de6e2d0dd5be79997884e5c7d812
Marian Milan <marian.milan6@gmail.com>
2025-08-01 11:20:09 +0200

    changed variable

--- src/main/java/model/figures/Piece.java
+++ src/main/java/model/figures/Piece.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public abstract class Piece {
 
-    private boolean white = false;
+    private final boolean white;
     private boolean moved = false;
     private Position position;
 
