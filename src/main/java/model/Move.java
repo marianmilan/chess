eb85a30a818ea9715ec783917301c6cac61d16ee
Marian Milan <marian.milan6@gmail.com>
2025-08-04 14:36:55 +0200

    new class for moves

--- src/main/java/model/Move.java
+++ src/main/java/model/Move.java
@@ -1,4 +1,30 @@
 package model;
 
+import model.figures.Piece;
+import model.figures.PieceType;
+
 public class Move {
+    public final Position from;
+    public final Position to;
+    public final Piece movedPiece;
+    public final Piece capturedPiece;
+    public PieceType promotingTo;
+    public boolean wasCastling;
+
+    public Move(Position from, Position to, Piece movedPiece, Piece capturedPiece, boolean wasCastling){
+        this.from = from;
+        this.to = to;
+        this.movedPiece = movedPiece;
+        this.capturedPiece = capturedPiece;
+        this.wasCastling = wasCastling;
+        this.promotingTo = null;
+    }
+
+    public void setPromotingTo(){
+        this.promotingTo = PieceType.QUEEN;
+    }
+
+    public boolean isPromoting(){
+        return promotingTo != null;
+    }
 }
