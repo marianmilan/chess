a80e17457b7ac9ad4141a0faecddafb8c8b5fb89
Marian Milan <marian.milan6@gmail.com>
2025-08-08 11:16:39 +0200

    refactor

--- src/main/java/logic/GameManager.java
+++ src/main/java/logic/GameManager.java
@@ -1,6 +1,7 @@
 package logic;
 
 import model.Board;
+import model.Move;
 import model.MoveResult;
 import model.Position;
 import model.figures.PieceType;
@@ -8,16 +9,21 @@ import model.figures.PieceType;
 public class GameManager {
     private Board board;
     private boolean whiteTurn;
+    private final ComputerPlayer computerPlayer;
 
     public GameManager(){
         board = new Board();
         whiteTurn = true;
+        computerPlayer = new ComputerPlayer();
     }
 
-
-    public MoveResult makeMove(Position start, Position targetSquare){
+    public MoveResult applyMove(Position start, Position targetSquare){
         MoveResult result = board.movePiece(start, targetSquare, whiteTurn);
+        if(!isWhiteTurn() && result == MoveResult.PROMOTION){
+            applyPromotion(PieceType.QUEEN, targetSquare.getPosX(), targetSquare.getPosY());
+        }
         if(result == MoveResult.VALID){
+            board.changeTurns();
             whiteTurn = !whiteTurn;
 
             if(board.staleMate(whiteTurn)){
@@ -28,11 +34,13 @@ public class GameManager {
                 return MoveResult.CHECK_MATE;
             }
         }
+        board.changeTurns();
         return result;
     }
 
-    public void makePromotion(PieceType type, int posX, int posY){
+    public void applyPromotion(PieceType type, int posX, int posY){
         board.promotion(type, whiteTurn, posX, posY);
+        board.changeTurns();
         whiteTurn = !whiteTurn;
     }
 
@@ -43,6 +51,11 @@ public class GameManager {
 
     public boolean isWhiteTurn(){
         return  whiteTurn;
+    }
+
+    public void computerMove(){
+        Move move = computerPlayer.findBestMove(board, 3);
+        applyMove(move.from, move.to);
     }
 
     public Board getBoard(){
